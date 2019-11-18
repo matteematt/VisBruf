501400e7f8b40f53151530aa633fc0cba130af28
Matt elementary <matt.walker@skillbranch.com>
2019-11-18 20:39:39 +0000

    Got main loop which can read up to 64 char at a time from the user

--- src/main.c
+++ src/main.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+#include "tickerDisplay.h"
+#include "dataTape.h"
 
 int main(int argc, char **argv)
 {
   printf("Hello Moto\n");
+
+  TickerDisplay display = {.mScrollDepth = 0, .mTickerHeight = 6}; 
+
+  DataTape data;
+  dt_DataTape(&data);
+
+  bool isRunning = true;
+
+  char inputBuff[64];
+
+  while (isRunning)
+  {
+    td_clearTTY();
+
+    printf("@ ");
+    
+    //Try and get input
+    if (fgets(inputBuff, sizeof inputBuff, stdin))
+    {
+
+    }
+    else
+    {
+      printf("Error getting input from user, exiting\n");
+    }
+  }
+
   return 0;
 }
