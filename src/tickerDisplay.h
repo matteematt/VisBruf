e6ef8d01f6cc9c4014156edc56fcb7dad1dc60ef
Matt elementary <matt.walker@skillbranch.com>
2019-11-23 17:09:30 +0000

    Basic implementation of printing out the data tape

--- src/tickerDisplay.h
+++ src/tickerDisplay.h
@@ -2,9 +2,15 @@
 #define TICKERDISPLAY
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 //For getting the terminal width
 #include <sys/ioctl.h>
 #include <unistd.h>
+#include <math.h>
+
+#include "dataTape.h"
+
 /**
   * The width of the terminal window in chars
   * The width of the display
@@ -21,6 +27,6 @@ typedef struct TickerDisplay{
 void td_clearTTY(void);
 
 //returns the number of rows the function has drawn
-int td_drawTicker(const TickerDisplay *dataTicker, const int dataIndex, const struct winsize *size);
+int td_drawTicker(const TickerDisplay *dataTicker, const DataTape *data, int ttyWidth);
 
 #endif
