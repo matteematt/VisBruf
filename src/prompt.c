21695a8b31b4acd0e940dfa8b9e95c37dcba42d4
Matt elementary <matt.walker@skillbranch.com>
2019-11-24 17:52:58 +0000

    Implemented the . brainfuck command

--- src/prompt.c
+++ src/prompt.c
@@ -1,17 +1,27 @@
 #include "prompt.h"
 
 //Private functions
-void parseInput(Prompt *prompt, DataTape *data);
 void parseInputCommand(DataTape *data);
+void pushOutputIndex(Prompt *prompt, DataTape *data);
 
+//Constructor
 void p_Prompt(Prompt *prompt)
 {
   prompt->mHeadPosition = 0;
   prompt->mHistoryLength = 16;
   prompt->mHistory = malloc(sizeof(Command) * prompt->mHistoryLength);
+  //Room for 128 char input and null byte
   prompt->mInputBuff = malloc(sizeof(char) * 129);
+  prompt->mOutputListLen = 8;
+  prompt->mOutputList = malloc(sizeof(int) * prompt->mOutputListLen);
+  for (int i = 0; i < prompt->mOutputListLen; i++)
+  {
+    prompt->mOutputList[i] = -1;
+  }
+  prompt->mOutputListIndex = -1;
 }
 
+//Destructor
 void p_DelPrompt(Prompt *prompt)
 {
   for (int i = 0; i < prompt->mHistoryLength; i++)
@@ -20,16 +30,18 @@ void p_DelPrompt(Prompt *prompt)
   }
   free(prompt->mHistory);
   free(prompt->mInputBuff);
+  free(prompt->mOutputList);
 }
 
-void p_runPrompt(Prompt *prompt, DataTape *data)
+//Reset output list and get user input into the buffer
+void p_getPromptInput(Prompt *prompt)
 {
   printf("@ ");
 
   //Try and get input
   if (fgets(prompt->mInputBuff, 128, stdin))
   {
-    parseInput(prompt, data);
+    //Input is parsed after the the screen is refreshed
   }
   else
   {
@@ -37,7 +49,7 @@ void p_runPrompt(Prompt *prompt, DataTape *data)
   }
 }
 
-void parseInput(Prompt *prompt, DataTape *data)
+void p_parseInput(Prompt *prompt, DataTape *data)
 {
   int i;
   for (i = 0; i <= 128 && prompt->mInputBuff[i] != '\0'; i++)
@@ -72,6 +84,10 @@ void parseInput(Prompt *prompt, DataTape *data)
         //Get a byte of input from the user and save it in the tape
         parseInputCommand(data);
         break;
+      case '.':
+        //Get the byte currently output and print it to stdout
+        pushOutputIndex(prompt, data);
+        break;
       default:
         //do nothing
         break;
@@ -79,9 +95,39 @@ void parseInput(Prompt *prompt, DataTape *data)
   }
 }
 
+//Print the output list pushed to from the last command, and reset as you go
+//Filled from the brainfuck '.' command
+void p_printOutputList(Prompt *prompt, DataTape *data)
+{
+  int i;
+  for (i = 0; i < prompt->mOutputListLen && prompt->mOutputList[i] != -1; i++)
+  {
+    putchar(data->mData[prompt->mOutputList[i]]);
+    prompt->mOutputList[i] = -1;
+  }
+  if (i > 0)
+  {
+    printf("\n");
+  }
+  prompt->mOutputListIndex = -1;
+}
+
+//Gets data for the ',' brainfuck command
 inline void parseInputCommand(DataTape *data)
 {
   printf("\n# ");
   char c = getchar();
   data->mData[data->mDataIndex] = c;
+}
+
+//Pushes index onto the output array for the '.' brainfuck command
+inline void pushOutputIndex(Prompt *prompt, DataTape *data)
+{
+  prompt->mOutputListIndex++;
+  if (prompt->mOutputListIndex == prompt->mOutputListLen)
+  {
+    prompt->mOutputListLen *= 2;
+    prompt->mOutputList = realloc(prompt->mOutputList, prompt->mOutputListLen);
+  }
+  prompt->mOutputList[prompt->mOutputListIndex] = data->mDataIndex;
 }
--- src/prompt.h
+++ src/prompt.h
@@ -11,16 +11,30 @@ typedef struct Command {
   unsigned int mStrLen;
 } Command;
 
+/**
+  * Dynamic history of commands so we can go back throughtr them
+  * the current max length of this dynamic array, wioll double in size when full
+  * the index of the current latests command
+  * A string which is the current input command, before its saved to the command array
+  * A list of ints which are indexes to put the chars after the prompt is run, -1 means dont print
+  * The max length of this dynamic int array, will double in size when full
+  * The current index that points to the most recently pushed output index
+  */
 typedef struct Prompt{
   Command *mHistory;
   int mHistoryLength;
   int mHeadPosition;
   char *mInputBuff;
+  int *mOutputList;
+  int mOutputListLen;
+  int mOutputListIndex;
 } Prompt;
 
 void p_Prompt(Prompt *prompt);
 void p_DelPrompt(Prompt *prompt);
 
-void p_runPrompt(Prompt *prompt, DataTape *data);
+void p_getPromptInput(Prompt *prompt);
+void p_parseInput(Prompt *prompt, DataTape *data);
+void p_printOutputList(Prompt *prompt, DataTape *data);
 
 #endif
