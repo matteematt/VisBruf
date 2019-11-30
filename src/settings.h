f805094e49d3f5cda1907badeb6ec0fc8d416cce
Matt elementary <matt.walker@skillbranch.com>
2019-11-30 17:01:46 +0000

    Added settings file which handles user settings and command line arguments

---
+++ src/settings.h
@@ -0,0 +1,31 @@
+#ifndef SETTINGS
+#define SETTINGS
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  * Holds current user settings about the prompt
+  * Some are set through command line arguments
+  */
+
+/**
+  * Pointer to a file to read in, else NULL
+  */
+typedef struct Settings 
+{
+  bool mIsError;
+  char *mReadFile;
+} Settings;
+
+//"Constructor/Destructor"
+void s_Settings(Settings *settings);
+void s_DelSettings(Settings *settings);
+
+//Parse command line arguments and update settings
+void s_ParseArgs(Settings *settings, int argc, char **argv);
+
+#endif
